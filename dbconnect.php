e7dd422f553da678556a27fa20d04bb51db821b0
Beem140 <66040233140@udru.ac.th>
2025-01-28 11:59:04 +0700

    new

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
 <!--<link href="css/style.css" rel="stylesheet">-->
 <?php
-$con = mysqli_connect("localhost", "its66040233140", "O1wvN2O5", "its66040233140") or die("เกิดข้อผิดพลาดเกิดขึ้น");
+$con = mysqli_connect("localhost", "root", "", "mydata") or die("เกิดข้อผิดพลาดเกิดขึ้น");
 mysqli_set_charset($con, "utf8");
